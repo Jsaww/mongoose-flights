4843b0848a1f39000ef0276a718592505f48a7a8
Jsaww <sawyerrjustin@gmail.com>
2022-08-08 21:53:32 -0700

    added new folders & files

--- config/database.js
+++ config/database.js
@@ -1,6 +1,13 @@
+// Point of this file is to create a connection to mongodb server running port 27017
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/flights', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+mongoose.connection.on('connected', function(){
+  console.log(`MongoDB Connected at 27017`)
+})
+
+
---
+++ models/flights.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+
+const flightSchema = new mongoose.Schema({
+
+
+})
